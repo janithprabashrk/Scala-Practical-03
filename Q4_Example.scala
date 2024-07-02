85b90bd62e9a4f476d62de7a91874d5ec8373106
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-02 08:20:08 +0530

    Final commit

--- Q4_Example.scala
+++ Q4_Example.scala
@@ -1,15 +1,16 @@
-object SumCalc{
-    def sumCalc(numbers : List[Int]) : Int = {
-        if (numbers.isEmpty){
-            0
-        }
-        else{
-            numbers.head + sumCalc(numbers.tail)
-        }
+object SumCalc {
+  def sumCalc(numbers: List[Int]): Int = {
+    if (numbers.isEmpty) {
+      0
+    } else if (numbers.head % 2 == 0) {
+      numbers.head + sumCalc(numbers.tail)
+    } else {
+      sumCalc(numbers.tail)
     }
+  }
 
-    def main(args: Array[String]) : Unit = {
-        val numbers = List(11, 52, 23, 34, 95)
-        println(sumCalc(numbers))
-    }
+  def main(args: Array[String]): Unit = {
+    val numbers = List(11, 52, 23, 34, 95)
+    println(sumCalc(numbers))
+  }
 }
---
+++ tempCodeRunnerFile.scala
@@ -0,0 +1,15 @@
+object SumCalc{
+    def sumCalc(numbers : List[Int]) : Int = {
+        if (numbers.isEmpty){
+            0
+        }
+        else if(numbers.head % 2 == 0){
+            numbers.head + sumCalc(numbers.tail)
+        }
+    }
+
+    def main(args: Array[String]) : Unit = {
+        val numbers = List(11, 52, 23, 34, 95)
+        println(sumCalc(numbers))
+    }
+}
